aaccfd00cb003ee6ddc008090dc93039aab780eb
gabrieljmacedo <gabrieljm58@hotmail.com>
2022-04-07 22:20:09 -0300

    implementacoes aula 3

--- aula-1-integracao/shiftweb6/src/main/java/br/com/fiap/shiftweb6/controller/UsuarioController.java
+++ aula-1-integracao/shiftweb6/src/main/java/br/com/fiap/shiftweb6/controller/UsuarioController.java
@@ -1,53 +1,142 @@
 package br.com.fiap.shiftweb6.controller;
 
+import java.net.URI;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import br.com.fiap.shiftweb6.model.UsuarioModel;
+import br.com.fiap.shiftweb6.repository.UsuarioRepository;
 
 @RestController
 @RequestMapping("/usuario")
 public class UsuarioController {
 
-    //metodos para os usuários
+    @Autowired
+    UsuarioRepository usuarioRepository;
+    //UsuarioRepository usuarioRepository = new UsuarioRepository();
 
-    private static final Object UsuarioModel = null;
 
-	@GetMapping
-    // findAll
-    public ResponseEntity<List<UsuarioModel>> findAll(){
 
-        List<UsuarioModel> lista = new ArrayList<>();
-        lista.add( new UsuarioModel("191", "fmoreni1@gmail.com.br") );
-        lista.add( new UsuarioModel("192", "fmoreni2@gmail.com.br") );
-        lista.add( new UsuarioModel("193", "fmoreni3@gmail.com.br") );
-
+    @GetMapping
+    public ResponseEntity<List<UsuarioModel>> findAll() {
+        List<UsuarioModel> lista = usuarioRepository.findAll();
         return ResponseEntity.ok(lista);
     }
 
+
     @GetMapping("/{id}")
-    // findById
     public ResponseEntity<UsuarioModel> findById(@PathVariable("id") Long id) {
+        UsuarioModel usuarioModel = usuarioRepository.findById(id).orElse(null);
 
-        if( id == 1 ) {
-            UsuarioModel usuarioModel = new UsuarioModel("191", "fmoreni1@gmail.com.br");
-
+        if ( usuarioModel != null  ) {
             return ResponseEntity.ok(usuarioModel);
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    @GetMapping("/{email}/{senha}")
+    public ResponseEntity<UsuarioModel> findByEmailAndSenha(
+            @PathVariable("email")	String email,
+            @PathVariable("senha")	String senha) {
+
+        return loginUsuario(email, senha);
+
+    }
+
+    @GetMapping("/login")
+    public ResponseEntity<UsuarioModel> login(
+            @RequestParam	String email,
+            @RequestParam	String senha) {
+
+        return loginUsuario(email, senha);
+    }
+
+    private ResponseEntity<UsuarioModel> loginUsuario(String email, String senha) {
+        UsuarioModel usuarioModel =
+                usuarioRepository.findByEmailAndSenha(email, senha);
+
+        if ( usuarioModel != null ) {
+            return ResponseEntity.ok(usuarioModel);
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+
+
+
+
+    @PostMapping
+    public ResponseEntity post(@RequestBody UsuarioModel usuarioModel) {
+        System.out.println(usuarioModel);
+
+        if ( null != usuarioModel.getIdade() ) {
+
+            usuarioRepository.save(usuarioModel);
+
+            URI location = ServletUriComponentsBuilder
+                    .fromCurrentRequest()
+                    .path("/{id}")
+                    .buildAndExpand(usuarioModel.getIdUsuario()).toUri();
+
+            return ResponseEntity.created(location).build();
+
+
+        } else {
+            return ResponseEntity.badRequest().body("Idade não informada");
+        }
+
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity put(@PathVariable("id") Long id,  @RequestBody UsuarioModel usuarioModel) {
+        System.out.println(usuarioModel);
+
+        if ( (null == id) || ( null == usuarioModel.getIdUsuario() ) ) {
+            return ResponseEntity.badRequest().body("id não informado");
+
+        } else if ( id != usuarioModel.getIdUsuario() ) {
+            return ResponseEntity.badRequest().body("id errado");
+
+        } else if ( null == usuarioModel.getIdade() ) {
+            return ResponseEntity.badRequest().body("idade não informada");
+
+        } else if ( ! usuarioRepository.existsById(id) ) {
+            return ResponseEntity.notFound().build();
+
+        } else {
+            usuarioModel.setIdUsuario(id);
+            usuarioRepository.save(usuarioModel);
+            return ResponseEntity.noContent().build();
+        }
+
+    }
+
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity delete(@PathVariable("id") Long id) {
+
+        if ( usuarioRepository.existsById(id) ) {
+            usuarioRepository.deleteById(id);
+            return ResponseEntity.noContent().build();
         } else {
             return ResponseEntity.notFound().build();
         }
 
     }
 
-    // post -- Criar
-
-    // put -- Alterar
-
-    // Delete
 }
--- aula-1-integracao/shiftweb6/src/main/java/br/com/fiap/shiftweb6/model/UsuarioModel.java
+++ aula-1-integracao/shiftweb6/src/main/java/br/com/fiap/shiftweb6/model/UsuarioModel.java
@@ -1,43 +1,65 @@
 package br.com.fiap.shiftweb6.model;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="SHIFT6_USUARIO")
 public class UsuarioModel {
 
-    //classes
+    @Id
+    @Column(name="ID_USUARIO")
+    @GeneratedValue( strategy = GenerationType.SEQUENCE, generator = "USUARIO_SEQ")
+    @SequenceGenerator( name="USUARIO_SEQ", initialValue = 1, allocationSize = 1)
     private Long idUsuario;
 
+    @Column(name="CPF")
     private String cpf;
 
+    @Column(name="NOME")
+    private String nome;
+
+    @Column(name="EMAIL")
     private String email;
 
+    @Column(name="SENHA")
     private String senha;
 
-    private long idade;
+    @Column(name="FOTO")
+    private String foto;
 
-    //constructor
+
+    @Column(name="IDADE")
+    private Long idade;
+
+
     public UsuarioModel() {
         super();
     }
 
     public UsuarioModel(String cpf, String email) {
+        super();
         this.cpf = cpf;
         this.email = email;
     }
 
-    public UsuarioModel(Long idUsuario, String cpf, String email) {
+    public UsuarioModel(Long idUsuario, String cpf, String nome, String email, String senha, Long idade) {
+        super();
         this.idUsuario = idUsuario;
         this.cpf = cpf;
-        this.email = email;
-    }
-
-    public UsuarioModel(Long idUsuario, String cpf, String email, String senha, long idade) {
-        this.idUsuario = idUsuario;
-        this.cpf = cpf;
+        this.nome = nome;
         this.email = email;
         this.senha = senha;
         this.idade = idade;
     }
 
-    // gt + setters
+
+
     public Long getIdUsuario() {
         return idUsuario;
     }
@@ -52,6 +74,14 @@ public class UsuarioModel {
 
     public void setCpf(String cpf) {
         this.cpf = cpf;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
     }
 
     public String getEmail() {
@@ -70,23 +100,28 @@ public class UsuarioModel {
         this.senha = senha;
     }
 
-    public long getIdade() {
+    public Long getIdade() {
         return idade;
     }
 
-    public void setIdade(long idade) {
+    public void setIdade(Long idade) {
         this.idade = idade;
     }
 
-    // to string
+    public String getFoto() {
+        return foto;
+    }
+
+    public void setFoto(String foto) {
+        this.foto = foto;
+    }
+
+
+
     @Override
     public String toString() {
-        return "UsuarioModel{" +
-                "idUsuario=" + idUsuario +
-                ", cpf='" + cpf + '\'' +
-                ", email='" + email + '\'' +
-                ", senha='" + senha + '\'' +
-                ", idade=" + idade +
-                '}';
+        return "UsuarioModel [idUsuario=" + idUsuario + ", cpf=" + cpf + ", nome=" + nome + ", email=" + email
+                + ", senha=" + senha + ", idade=" + idade + "]";
     }
+
 }
