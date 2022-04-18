a7b525749b2da69dc8897fecf8bd5d538d660d0c
gabrieljmacedo <gabrieljm58@hotmail.com>
2022-04-18 19:31:57 -0300

    correcão repositórios

--- aula-1-integracao/shiftweb6/src/main/java/br/com/fiap/shiftweb6/model/CategoriaModel.java
+++ aula-1-integracao/shiftweb6/src/main/java/br/com/fiap/shiftweb6/model/CategoriaModel.java
@@ -1,66 +1,72 @@
 package br.com.fiap.shiftweb6.model;
 
-import javax.persistence.*;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
 
 @Entity
-@Table(name = "SHIFT6_CATEGORIA")
+@Table(name="SHIFT6_CATEGORIA")
 public class CategoriaModel {
 
     @Id
     @Column(name="ID_CATEGORIA")
-    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "CATEGORIA_SEQ")
-    @SequenceGenerator(name = "CATEGORIA_SEQ", initialValue = 10, allocationSize = 1)
-    private long idCategoria;
+    @GeneratedValue( strategy = GenerationType.SEQUENCE, generator = "CATEGORIA_SEQ")
+    @SequenceGenerator( name = "CATEGORIA_SEQ", initialValue = 1, allocationSize = 1)
+    private Long idCategoria;
 
-    @Column(name = "NOME_CATEGORIA")
-    private String nome;
+    @Column(name="NOME_CATEGORIA")
+    private String nomeCategoria;
 
-    @Column(name = "ATIVO")
-    private boolean ativo;
+    @Column(name="ATIVO")
+    private Boolean ativo;
 
-    // CONSTRUTORES
-    public CategoriaModel(){
+
+
+    public CategoriaModel() {
         super();
     }
 
-    public CategoriaModel(long idCategoria, String nome, boolean ativo) {
+    public CategoriaModel(Long idCategoria, String nomeCategoria) {
         super();
         this.idCategoria = idCategoria;
-        this.nome = nome;
-        this.ativo = ativo;
+        this.nomeCategoria = nomeCategoria;
     }
 
-    // GT + ST
-    public long getIdCategoria() {
+
+
+    public Long getIdCategoria() {
         return idCategoria;
     }
 
-    public void setIdCategoria(long idCategoria) {
+    public void setIdCategoria(Long idCategoria) {
         this.idCategoria = idCategoria;
     }
 
-    public String getNome() {
-        return nome;
+    public String getNomeCategoria() {
+        return nomeCategoria;
     }
 
-    public void setNome(String nome) {
-        this.nome = nome;
+    public void setNomeCategoria(String nomeCategoria) {
+        this.nomeCategoria = nomeCategoria;
     }
 
-    public boolean isAtivo() {
+
+    public Boolean getAtivo() {
         return ativo;
     }
 
-    public void setAtivo(boolean ativo) {
+    public void setAtivo(Boolean ativo) {
         this.ativo = ativo;
     }
 
     @Override
     public String toString() {
-        return "CategoriaModel{" +
-                "idCategoria=" + idCategoria +
-                ", nome='" + nome + '\'' +
-                ", ativo=" + ativo +
-                '}';
+        return "CategoriaModel [idCategoria=" + idCategoria + ", nomeCategoria=" + nomeCategoria + "]";
     }
+
+
 }
